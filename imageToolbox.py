99e04daf560e861cf8f114c8257f8259f927a2c8
Colin Baxter <cwbaxter@uw.edu>
2023-10-27 16:40:20 -0700

    In the process of adding doc strings to functions for this section.

--- imageToolbox.py
+++ imageToolbox.py
@@ -4,30 +4,65 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
-def cropPhotos(photosLocation, yMinC, yMaxC, xMinC, xMaxC):
+def cropPhotos(photos_location, y_min, y_max, x_min, x_max):
+    """Crops all photos in a directory based on the (x,y) coordinates of the crop area.
+    Creates a folder within the directory titled '/Cropped' containing the cropped images 
+
+    Parameters
+    ----------
+    photos_location : str
+        Directory that contains the folders with the images
+    y_min : int
+        The minimum y location of the crop area (bottom left y-coordinate)
+    y_max : int
+        The maximum y location of the crop area (top right y-coordinate)
+    x_min : int
+        The minimum x location of the crop area (bottom left x-coordinate)
+    x_max : int
+        The maximum x location of the crop area (top right x-coordinate)
+    
+
+    Returns
+    -------
+    None
+    """
+    
     #Create a folder to store the cropped photos
-    os.makedirs(photosLocation + '/Cropped')
-    cropAddress = os.path.join(photosLocation, 'Cropped')
+    os.makedirs(photos_location + '/Cropped')
+    cropAddress = os.path.join(photos_location, 'Cropped')
 
     #https://stackoverflow.com/questions/17291455/how-to-get-an-average-picture-from-100-pictures-using-pil
     #Grab all files within the folder location (imgLocation)
-    allfiles=os.listdir(photosLocation)
+    allfiles=os.listdir(photos_location)
     imlist=[filename for filename in allfiles if  filename[-4:] in [".jpg",".JPG"]]
 
     for im in imlist:
-        originalIm = mpimg.imread(os.path.join(photosLocation, im))
-        croppedIm = originalIm[yMinC:yMaxC, xMinC:xMaxC]
+        originalIm = mpimg.imread(os.path.join(photos_location, im))
+        croppedIm = originalIm[y_min:y_max, x_min:x_max]
         #cv2.imwrite(os.path.join(photosLocation, '\Cropped', im), croppedIm)
         cv2.imwrite(os.path.join(cropAddress, im), croppedIm)
 
-def imgAverage(imgLocation):
+def imgAverage(images_location):
+   """Performs element-wise averaging of the images within the input directory.  
+
+    Parameters
+    ----------
+    images_location : str
+        Directory that contains the folders with the images
+    
+
+    Returns
+    -------
+    None
+    """
+   
     #https://stackoverflow.com/questions/17291455/how-to-get-an-average-picture-from-100-pictures-using-pil
     #Grab all files within the folder location (imgLocation)
-    allfiles=os.listdir(imgLocation)
+    allfiles=os.listdir(images_location)
     imlist=[filename for filename in allfiles if  filename[-4:] in [".jpg",".JPG"]]
 
     #All pictures are the same size, so grab the dimenions of the first image
-    w,h=Image.open(os.path.join(imgLocation, imlist[0])).size
+    w,h=Image.open(os.path.join(images_location, imlist[0])).size
     N=len(imlist)
 
     #Initialize an array to hold the average, 3rd index of zeros is 1 because these are CCD
@@ -35,26 +70,50 @@ def imgAverage(imgLocation):
 
     #Add each image to an array and average them
     for im in imlist:
-        imarr = np.array(Image.open(os.path.join(imgLocation, im)),dtype=float)
+        imarr = np.array(Image.open(os.path.join(images_location, im)),dtype=float)
         arr=arr+imarr/N
 
     #Round the value of each float and cast it to 8-bit integer
     arr=np.array(np.round(arr),dtype=np.uint8)
     return arr
 
-def createCorrectionImages(dirname):
+def createCorrectionImages(folders_location):
+    """Applies a flat-field correction to the images within the directory
+
+    ## Prerequisites
+    1. Create a folder 'Corrections\' within the directory
+    2. Include folders named 'Ambient' 'FlatField' and 'DarkNoise' for each of the 3 types
+    and include the photos within them.
+    3. Function 'imgAverage' must be run on the folders listed in step 2
+
+    Parameters
+    ----------
+    folders_location : str
+        Directory that contains the folders with the images
+        
+
+    Returns
+    -------
+    ambient_image : image
+        averaged ambient image
+    flat_field_image : image
+        averaged flat field image
+    dark_noise_image : image
+        averaged dark noise image
+    """
+    
     # Function that generates the ambient, flat-field, and dark-noise images for the image correction
     # Designed to be only run once to save computational space
     #Image Correction Files
-    ambientFolder = os.path.join(dirname, 'Corrections\Ambient\Cropped')
-    ffcFolder = os.path.join(dirname, 'Corrections\FlatField\Cropped')
-    darkFolder = os.path.join(dirname, 'Corrections\DarkNoise\Cropped')
+    ambientFolder = os.path.join(folders_location, 'Corrections\Ambient\Cropped')
+    ffcFolder = os.path.join(folders_location, 'Corrections\FlatField\Cropped')
+    darkFolder = os.path.join(folders_location, 'Corrections\DarkNoise\Cropped')
 
     #Create the average corrected images
-    ambImg = imgAverage(ambientFolder)
-    ffImg = imgAverage(ffcFolder)
-    darkImg = imgAverage(darkFolder)
-    return ambImg, ffImg, darkImg
+    ambient_image = imgAverage(ambientFolder)
+    flat_field_image = imgAverage(ffcFolder)
+    dark_noise_image = imgAverage(darkFolder)
+    return ambient_image, flat_field_image, dark_noise_image
 
 def flatFieldCorrection(raw, flat, dark):
     FmD = flat - dark
