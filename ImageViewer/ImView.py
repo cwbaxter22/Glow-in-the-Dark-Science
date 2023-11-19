ff507f145314c03ffdc39e866a6d25744a7ca0ae
Colin Zuckerberg Baxter <cyb5201@psu.edu>
2023-11-18 20:52:04 -0800

    Switched from plotly to matplotlib in ImView.py to make it easy to convert using PyInstaller. ImageViewer.spec data field modified to include path to matplotlib. If PyInstaller is used again, this address will need updated.

---
+++ ImageViewer/ImView.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+#import plotly.express as px
+#import cv2
+
+def clicked():
+    '''Simple python function to preview image intensities on older PC that does not have python installed.'''
+    file_path = filedialog.askopenfilename()
+    lbl.configure(text = file_path)
+    #rawImage = cv2.imread(file_path)
+    #fig = px.imshow(rawImage)
+    #fig.show()
+
+    img = mpimg.imread(file_path)
+    plt.imshow(img)
+    plt.show()
+ 
+# create root window
+root = tk.Tk()
+
+btn = Button(root, text = "Browse" ,
+             fg = "blue", command=clicked)
+# set Button grid
+btn.pack()
+
+lbl = Label(root, text = "Choose Image")
+lbl.pack()
+ 
+# root window title and dimension
+root.title("Image Intensity Preview")
+
+# Set geometry (widthxheight)
+root.geometry('350x200')
+ 
+#Keep the window
+root.mainloop()
+
+
+
---
+++ ImageViewer/ImageViewer.spec
@@ -0,0 +1,37 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+
+a = Analysis(
+    ['ImView.py'],
+    pathex=[],
+    binaries=[],
+    datas=[('C:\Python312\Lib\site-packages\matplotlib\\', 'matplotlib')],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+)
+pyz = PYZ(a.pure)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.datas,
+    [],
+    name='ImageViewer',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
